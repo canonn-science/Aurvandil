f2046114fa35052611b00a62917b312a031b3b53
NoFoolLikeOne <nofool@paparomeo.uk>
2019-07-29 23:28:15 +0100

    Fixing scrolling text and adding new pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,11 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+                            'images/bg01.png': 'center',
+							'images/bg02.png': 'center',
+                            'images/bg02.jpg': 'center',                            
+							'images/bg03.png': 'center',                            
+                            'images/bg04.png': 'center'
 						},
 
 					// Delay.
